64fafd9a2bedd919525389a13c860e8fa526a6ac
Ahmed Hossam <1318039@gmail.com>
2022-01-17 22:12:20 +0200

    Edit home page style

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,23 @@
-import { graphql } from "gatsby";
 import * as React from "react";
+import { graphql } from "gatsby";
 import Layout from "../components/Layout";
+import PostCard from "../components/PostCard";
+import Info from "../components/Info";
 
 const IndexPage = ({ data }) => {
   console.log(data);
 
   return (
-    <Layout>
-      <p className="bg-red-500">Hello World</p>
+    <Layout pageTitle="Home Page">
+      <div className="container mt-12">
+        <Info />
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-12 mt-16 container mb-12">
+        {data.allMdx.nodes.map((node) => (
+          <PostCard post={node} />
+        ))}
+      </div>
     </Layout>
   );
 };
@@ -19,6 +29,12 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          hero_image {
+            childImageSharp {
+              gatsbyImageData(width: 600)
+            }
+          }
+          hero_image_alt
         }
         id
         slug
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "8rem",
+        xl: "16rem",
+        "2xl": "24rem",
+      },
+      center: true,
+    },
   },
   plugins: [],
 };
