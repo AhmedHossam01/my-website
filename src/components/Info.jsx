780995ca82ebe368db95b04e895c3db1c74b9aea
Ahmed Hossam <1318039@gmail.com>
2022-02-27 18:49:04 +0200

    Change CTAs

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -19,34 +19,36 @@ const Info = () => {
         <ul className="mt-4 flex gap-4 items-center">
           <li>
             <a
-              href="https://www.twitter.com/devahmedhossam"
+              href="https://www.linkedin.com/in/ahmedhossam01/"
               target="_blank"
-              rel="noreferrer"
-              className="flex gap-2 text-white bg-blue-600 hover:bg-blue-800 transition px-2 py-1 rounded-md"
+              rel="noopener noreferrer"
+              className="inline-flex items-center bg-sky-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-sky-700 transition-colors rounded text-base mt-4 md:mt-0"
             >
-              Follow me on Twitter
               <svg
-                class="w-6 h-6 text-blue-100 fill-current transition-all"
                 xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
                 viewBox="0 0 24 24"
+                className="fill-current mr-2 w-5 h-5"
               >
-                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
               </svg>
+              Connect on LinkedIn
             </a>
           </li>
 
           <li>
             <a
-              href="https://www.linkedin.com/in/ahmedhossam01/"
+              href="https://twitter.com/devahmedhossam"
               target="_blank"
               rel="noreferrer"
             >
               <svg
-                class="w-6 h-6 text-blue-500 fill-current hover:text-blue-700 transition-all"
+                class="w-6 h-6 text-slate-300 fill-current hover:text-white transition-all"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
+                viewBox="0 0 24 24"
               >
-                <path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+                <path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" />
               </svg>
             </a>
           </li>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,28 +35,39 @@ const Navbar = ({ setIsDark, isDark }) => {
             Home
           </Link>
 
-          {/* <Link
-            className="mr-5 hover:text-gray-900 dark:hover:text-white"
-            to="/"
+          <a
+            className="mr-5 hover:text-gray-900 dark:hover:text-white flex gap-2 group"
+            href="/youtube"
           >
-            Nothing Interesting
-          </Link> */}
+            Recommended Dev Channels
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="mr-2 text-gray-500 group-hover:text-red-500"
+            >
+              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+            </svg>
+          </a>
         </nav>
         <a
-          href="/youtube"
-          className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0 dark:bg-slate-700 dark:text-white"
+          href="https://www.linkedin.com/in/ahmedhossam01/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-flex items-center bg-sky-500 text-white border-0 py-1 px-3 focus:outline-none hover:bg-sky-700 transition-colors rounded text-base mt-4 md:mt-0"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="currentColor"
-            className="mr-2 text-red-500"
+            className="fill-current mr-2 w-5 h-5"
           >
-            <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
           </svg>
-          Recommend Dev Channels
+          Connect on LinkedIn
         </a>
 
         <button
